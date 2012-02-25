cbd969135e8ec8c08fcccfe1ca96775c1c77ccac
Julien Rioux <julien.rioux@uni-konstanz.de>
2012-02-25 18:46:21 +0100

    Allow to specify issue numbers on the command line for testing.
    If no issue number is provided, query for Patch=new issues, as usual.

--- patches/test-patches.py
+++ patches/test-patches.py
@@ -3,10 +3,15 @@
 import sys
 import projecthosting_patches
 
-def main():
+def main(issues_id = None):
     patchy = projecthosting_patches.PatchBot()
-    patchy.do_new_check()
+    if issues_id:
+        issues = patchy.get_issues(issues_id)
+    else:
+        issues = patchy.get_new_patches()
+    patchy.do_check(issues_id)
 
 if __name__ == "__main__":
-    main()
+    issues_id = sys.argv[1:]
+    main(issues_id)
 
